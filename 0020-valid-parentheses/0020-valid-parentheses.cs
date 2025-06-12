c2af804909b2696c907c667de2169f1ef53bb3b8
Muhammad Nehal <55832972+knehal@users.noreply.github.com>
2025-06-12 12:01:19 +0500

    Time: 1 ms (98.64%), Space: 42.4 MB (18.19%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
+            else if (c == ')' || c == ']' || c == '}')
+            {
+                if (stack.Count == 0)
+                    return false;
+
+                char top = stack.Pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{'))
+                    return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+}
